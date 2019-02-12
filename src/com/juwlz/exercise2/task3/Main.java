9edc5ac46a2d8f2c1f6a222c6c7b5448e7c92e10
Julius Soutine <jsoutine@gmail.com>
2019-02-12 12:51:57 +0100

    trying to merge

--- src/com/juwlz/exercise2/task3/Main.java
+++ src/com/juwlz/exercise2/task3/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Main myApp = new Main();
+        MyTimer timer = new MyTimer();
+
+        while(true){
+            try {
+                Thread.sleep(1000);
+                //notify observer
+                //add some code
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
